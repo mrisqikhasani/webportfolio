6d7104c29b5e5d39d2b68daf35a4ea601a09372f
mrisqikhasani <mrisqikhasani@gmail.com>
2025-01-13 05:49:20 +0700

    tugas website

--- js/autotyping.js
+++ js/autotyping.js
@@ -119,7 +119,7 @@ var AutoTyping = (function (e) {
   ]).default;
   
   
-  const exampleText = ['Web Developer', 'IT Support', 'ML Developer'];
+  const exampleText = ['Web Developer', 'IT Support', 'ML Developer', 'System Administrator'];
   const exampleTyping = new AutoTyping('#text', exampleText, {
       typeSpeed: 100,
       deleteSpeed: 100,
